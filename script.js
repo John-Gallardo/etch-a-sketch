5c3a4107320ac0bd7da00e66f8c4257ee8e59f0a
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-08-07 00:04:44 -0600

    Feat: Finished 4. (Added button to prompt for new grid)

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 /* HTML / CSS Logic */
-const gridSize = 16;
+let gridSize = 16;
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
+const containerSize = container.clientWidth;
 
-function createGrid() {
+function createStartGrid() {
     let squareDiv;
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
@@ -13,15 +15,39 @@ function createGrid() {
     }
 }
 
+function createGrid() {
+    gridSize = prompt("New Grid Size (1-100):");
+    if (gridSize > 100 || gridSize < 1)
+        alert("Error: Grid Size too big or too small");
+    else {
+        deleteGrid();
+        pixelSize = containerSize / gridSize;
+        for (let i = 0; i < gridSize; i++) {
+            for (let j = 0; j < gridSize; j++) {
+                squareDiv = document.createElement("div");
+                squareDiv.setAttribute("style", `width: ${pixelSize}px; height: ${pixelSize}px`);
+                container.appendChild(squareDiv);
+            }
+        }
+    }
+}
+
 function containerLogic(event) {
     let hoverTarget = event.target;
     if (hoverTarget != container)
         hoverTarget.classList.add("hovered");
 }
 
+function deleteGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
+/* Events */
 container.addEventListener("mouseover", containerLogic);
+button.addEventListener("click", createGrid);
 
 /* Function Calls */
 
-createGrid();
+createStartGrid();
